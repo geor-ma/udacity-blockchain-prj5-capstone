97d45fcfd095d779b4be10406ec7e9c22a6f7d1c
manishG <mg.work.livn@gmail.com>
2021-10-04 10:36:33 +0000

    more setup

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 Setup when you need them.
 
 ```bash
-# cd to contracts directory and run commands
+
+$ npm init
 $ npm install --save  openzeppelin-solidity@2.3
 $ npm install --save  truffle-hdwallet-provider@1.0.17
 $ npm audit fix
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const SquareVerifier = artifacts.require("SquareVerifier.sol");
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
+  deployer.deploy(SquareVerifier);
+  deployer.deploy(SolnSquareVerifier);
 };
