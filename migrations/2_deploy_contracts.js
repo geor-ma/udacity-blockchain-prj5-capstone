ec920c3ed6bcfdf84d8363ee0a1872b38268face
manishG <mg.work.livn@gmail.com>
2021-10-13 03:54:51 +0000

    Deploy latest contracts generated by Zokrates (a.k.a verifier.sol)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,12 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
-// const Verifier = artifacts.require("Verifier.sol");
-// const SolnSquareVerifier = artifacts.require("SolnSquareVerifier.sol");
+const Verifier = artifacts.require("Verifier.sol");
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier.sol");
 
 module.exports = async (deployer) => {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-
-  // await deployer.deploy(Verifier);
-  // await deployer.deploy(
-  //   SolnSquareVerifier,
-  //   Verifier.address,
-  //   "ERC721MintableToken",
-  //   "MG-TKN-007"
-  // );
+  await deployer.deploy(Verifier);
+  await deployer.deploy(
+    SolnSquareVerifier,
+    Verifier.address,
+    "ERC721MintableToken",
+    "MG-TKN-007"
+  );
 };
